dbb673c650551f58c5dec3048d20a80175e1d4de
VegetableBirddd <wu13540666210@163.com>
2024-03-01 16:16:55 +0800

    add:sample of react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  useEffect(() => {
+    console.log('effect')
+    const timer = setInterval(() => {
+      console.log(num);
+    }, 1000);
+
+    return () => {
+      console.log('clean up')
+      clearInterval(timer);
+    }
+  }, [num]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
   );
 }
 
---
+++ src/App3.tsx
@@ -0,0 +1,49 @@
+import { produce } from "immer";
+import { Reducer, useReducer } from "react";
+
+interface Data {
+    result: number;
+}
+
+interface Action {
+    type: 'add' | 'minus',
+    num: number
+}
+function reducer(state: Data, action: Action) {
+
+    switch(action.type) {
+        case 'add':
+            return produce(state,(state)=>{
+                state.result += action.num
+            })
+            // return {
+            //     result: state.result + action.num
+            // }
+        case 'minus': 
+            return {
+                result: state.result - action.num
+            }
+    }
+    return state;
+}
+
+function App() {
+  const [res, dispatch] = useReducer<Reducer<Data, Action>>(reducer, { result: 0});
+  
+//第二种写法
+//   const [res, dispatch] = useReducer<Reducer<Data, Action>, string>(reducer, 'zero', (param) => {
+//     return {
+//         result: param === 'zero' ? 0 : 1
+//     }
+// });
+
+  return (
+    <div>
+        <div onClick={() => dispatch({ type: 'add', num: 2 })}>加</div>
+        <div onClick={() => dispatch({ type: 'minus', num: 1 })}>减</div>
+        <div>{res.result}</div>
+    </div>
+  );
+}
+
+export default App;
