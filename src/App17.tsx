c99761f0303d408fc01b7156951e70027d76d4b2
VegetableBirddd <wu13540666210@163.com>
2024-06-04 16:25:29 +0800

    add:lazyload component

---
+++ src/App17.tsx
@@ -0,0 +1,45 @@
+import img1 from './img/vite.png';
+import img2 from './img/webpack.png'
+import LazyLoad from 'react-lazyload';
+
+export default function App() {
+  return (
+    <div>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <p>xxxxxx</p>
+      <LazyLoad placeholder={<div>loading...</div>}>
+        <img src={img1}/>
+      </LazyLoad>
+      <LazyLoad placeholder={<div>loading...</div>}>
+        <img src={img2}/>
+      </LazyLoad>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App15';
+import App from './App17';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
